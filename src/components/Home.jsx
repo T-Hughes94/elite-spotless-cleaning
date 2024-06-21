056babcf698883850d4480ab7ad6ded10f424c1a
Terence Hughes <Terencehughes450@gmail.com>
2024-06-20 20:24:56 -0400

    fixing home screen sizng

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,16 +15,16 @@ const Home = () => {
     <div id="home" className="relative w-full h-screen flex items-center justify-center bg-cover bg-center"
       style={{ 
         backgroundImage: `url(${ecsLogo})`,
-        backgroundSize: '90% 140%', // Adjust the background size here
+        backgroundSize: 'contain', // Adjust the background size here
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <div className="bg-white/70 p-8 rounded-lg shadow-lg max-w-3xl text-center">
+      <div className="bg-white/70 p-8 rounded-lg shadow-lg max-w-3xl text-center mx-4 md:mx-auto">
         <h1 className="text-4xl font-bold mb-4 text-gray-800">Elite Spotless Cleaning</h1>
-        <p className="text-2xl text-gray-950 mb-4">
+        <p className="text-xl md:text-2xl text-gray-950 mb-4">
           Elite Spotless Cleaning offers a unique and unparalleled service. Our primary focus is on exceeding our clients' expectations by consistently going the extra mile for you.
         </p>
-        <p className="text-2xl text-gray-850 mb-4">
+        <p className="text-xl md:text-2xl text-gray-850 mb-4">
           As a well-established and highly skilled cleaning service provider, we pride ourselves on delivering exceptional customer service and top-tier cleaning solutions. We are a dedicated team of professionals who cater to the requirements of our esteemed clientele residing in the New York City Region.
         </p>
         <h2 className="justify-center flex sm:text-3xl text-2xl pt-4 text-gray-800">
@@ -53,4 +53,5 @@ const Home = () => {
 };
 
 export default Home;
+
 
