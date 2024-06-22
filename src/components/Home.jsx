1796d225e28408e4826dae944d317547b69bc131
Terence Hughes <Terencehughes450@gmail.com>
2024-06-21 22:20:24 -0400

    made home lettering bold, added new favicon, increased size of first header text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,11 +20,11 @@ const Home = () => {
       }}
     >
       <div className="bg-white/70 p-8 rounded-lg shadow-lg max-w-3xl text-center mx-4 md:mx-auto">
-        <h1 className="text-4xl font-bold mb-4 text-gray-800">Elite Spotless Cleaning</h1>
-        <p className="text-xl md:text-2xl text-gray-950 mb-4">
+        <h1 className="text-5xl font-bold mb-4 text-gray-800">Elite Spotless Cleaning</h1>
+        <p className="text-xl font-bold md:text-2xl text-gray-950 mb-4">
           Elite Spotless Cleaning offers a unique and unparalleled service. Our primary focus is on exceeding our clients' expectations by consistently going the extra mile for you.
         </p>
-        <p className="text-xl md:text-2xl text-gray-850 mb-4">
+        <p className="text-xl font-bold md:text-2xl text-gray-950 mb-4">
           As a well-established and highly skilled cleaning service provider, we pride ourselves on delivering exceptional customer service and top-tier cleaning solutions. We are a dedicated team of professionals who cater to the requirements of our esteemed clientele residing in the New York City Region.
         </p>
         {/* Button to navigate to the quote form */}
