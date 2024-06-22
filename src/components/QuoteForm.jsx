e0052c518f9ff8a602b0091f3f2c53d2106ff528
Terence Hughes <Terencehughes450@gmail.com>
2024-06-22 13:20:08 -0400

    corrected submission form

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -109,7 +109,7 @@ function QuoteForm() {
 
   const sendEmails = async (templateParams, estimatedQuoteValue) => {
     try {
-    
+     
       await emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, templateParams);
       console.log('Email to service provider sent successfully');
       setShowConfirmation(true);
@@ -314,7 +314,7 @@ function QuoteForm() {
               Thank you for your request!
             </p>
             <p className='py-4'>
-             contact you shortly to  We have received your submission and will confirm the details and schedule your cleaning.
+            We have received your submission and will contact you shortly to confirm the details and schedule your cleaning.
             </p>
             <p className='py-4'>
               Your estimated quote is: <strong className='text-xl text-red-500'>{estimatedQuote}</strong>
