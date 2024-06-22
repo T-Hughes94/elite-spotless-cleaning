901eeb50c11ac861c5e1189c51506af57cc1151c
Terence Hughes <Terencehughes450@gmail.com>
2024-06-22 13:08:50 -0400

    made changes to input fields on quote form for number of specialists input and  time needed input changed to time for job input

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -15,7 +15,7 @@ const initialState = {
   startDate: new Date(),
   startTime: '09:00',
   numberOfSpecialists: '1',
-  estimatedTimeNeeded: '',
+  estimatedTimeForJob: '',
   additionalInfo: ''
 };
 
@@ -67,7 +67,7 @@ function QuoteForm() {
   };
 
   const calculateEstimatedQuote = () => {
-    const hours = parseInt(formState.estimatedTimeNeeded, 10);
+    const hours = parseInt(formState.estimatedTimeForJob, 10);
     let hourlyRate = 0;
 
     switch (formState.serviceCategory) {
@@ -109,7 +109,7 @@ function QuoteForm() {
 
   const sendEmails = async (templateParams, estimatedQuoteValue) => {
     try {
-      // Deliberately using incorrect service ID for testing error handling
+    
       await emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, templateParams);
       console.log('Email to service provider sent successfully');
       setShowConfirmation(true);
@@ -262,7 +262,7 @@ function QuoteForm() {
                 />
               </div>
               <div className='flex flex-col'>
-                <label className='uppercase text-sm font-semibold py-2'>Number of Specialists</label>
+                <label className='uppercase text-sm font-semibold py-2'>Number of Specialists (Max 2)</label>
                 <input
                   className='border-2 rounded-lg p-3 border-gray-300 focus:border-[#001b5e] focus:ring-[#001b5e]'
                   type='number'
@@ -276,11 +276,11 @@ function QuoteForm() {
                 />
               </div>
               <div className='flex flex-col'>
-                <label className='uppercase text-sm font-semibold py-2'>Estimated Time Needed (Hours)</label>
+                <label className='uppercase text-sm font-semibold py-2'>Estimated Time for Job (Hours)</label>
                 <input
                   type='number'
-                  name='estimatedTimeNeeded'
-                  value={formState.estimatedTimeNeeded}
+                  name='estimatedTimeForJob'
+                  value={formState.estimatedTimeForJob}
                   onChange={handleChange}
                   min='1'
                   max='99'
